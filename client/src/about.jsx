5409b62134fe14fd9167f6de9c9a83ddc3513c10
Jordan Than <jordanthan7@gmail.com>
2024-05-24 23:53:02 -0400

    initial commit

---
+++ client/src/about.jsx
@@ -0,0 +1,18 @@
+/*
+File: about.jsx
+Student: Jordan Than
+ID: 301360824
+Date: 05/21/24
+*/
+export default function About() {
+    return (
+        <>
+            <h1>About Me</h1>
+            <img src="./src/images/me.jpg" alt="picture of me" width={750} height={1000}></img>
+            <p>My full name is Jordan Wai-Lun Than, and I'm a Canadian-born Web Developer based in Ontario. 
+            Currently, I'm a student studying the Software Engineering Technican Program at Centennial College.
+            </p>
+            <p>My Resume</p>
+        </>
+    );
+}    
