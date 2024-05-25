30d412fd34941e05069957c482353366d3e25194
Jordan Than <jordanthan7@gmail.com>
2024-05-25 00:02:55 -0400

    initial commit

---
+++ client/components/Home.jsx
@@ -0,0 +1,17 @@
+/*
+File: Home.jsx
+Student: Jordan Than
+ID: 301360824
+Date: 05/22/24
+*/
+export default function Home() {
+    return ( 
+        <>
+            <p>Welcome to my Portfolio website! I hope by the end you get to know me better.</p>
+            <h2>Mission Statement:</h2>
+            <p>My mission as a programmer is to output a consistent performance equally motivated and passionate in each project.
+            I'll be flexible and be able to adapt.</p>
+        </>
+    );
+}    
+//<b><link href="/about.jsx">Click Here To Learn More About Me</link></b>
---
+++ client/src/contact.jsx
@@ -0,0 +1,28 @@
+export default function Contact() {
+    return (
+        <>
+            <h1>How to Contact Me</h1>
+            <p>Phone Number: +1 437 249-1806 <br></br> 
+            Email Address: jordanthan7@gmail.com</p>
+            <h2>Send a Message</h2>
+            <form method="post">
+                <label>First Name: </label>
+                <input type="text" placeholder="First Name*"></input>
+                <br></br><br></br>
+                <label>Last Name:</label>
+                <input type="text" placeholder="Last Name*"> </input>
+                <br></br><br></br>
+                <label>Contact Number:</label>
+                <input type="number" placeholder="Contact Number*"></input>
+                <br></br><br></br>
+                <label>Email Address:</label>
+                <input type="text" placeholder="Email Address"></input>
+                <br></br><br></br>
+                <label>Message:</label>
+                <textarea type="text" placeholder="Message*"></textarea>
+                <br></br><br></br>
+                <input type="submit"></input>
+            </form>
+        </>
+    );
+}
