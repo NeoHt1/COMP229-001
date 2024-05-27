2b4ce9ee0444c22083cb5707076ab334b8aa489b
Jordan Than <jordanthan7@gmail.com>
2024-05-26 21:52:31 -0400

    commit

--- client/components/Layout.jsx
+++ client/components/Layout.jsx
@@ -18,8 +18,8 @@ export default function Layout() {
         <nav>
             <Link to="/">Home</Link> | <Link to="/about">About Me</Link> | <Link to="/services">Services</Link> | <Link to="/project">Project</Link> | <Link to="/contact">Contact</Link>
         </nav>
-        <br/>
-      <hr />
+        <br/><br/>
+        <hr/>
     </>
     );
 }
